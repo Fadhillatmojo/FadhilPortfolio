965eaf04c7678261bcb0842157bcf817f6011d74
User <fadhillatmojo@gmail.com>
2023-11-14 20:36:30 +0700

    fix/fitur lightbox

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -16,7 +16,7 @@
 	@foreach ($galleries as $gallery)
 	<div class="col-sm-2">
 		<div>
-			<a class="example-image-link" href="{{$gallery->original_pict}}" data-lightbox="roadtrip"
+			<a class="example-image-link" href="{{asset('storage/posts_image/'.$gallery->picture )}}" data-lightbox="roadtrip"
 				data-title="{{$gallery->description}}">
 				<img class="example-image img-fluid mb-2"src="{{asset('storage/posts_image/'.$gallery->picture )}}" alt="image-1" />
 			</a>
