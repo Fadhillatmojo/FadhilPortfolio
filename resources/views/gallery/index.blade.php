4899db74e9ad76eea56b28b18f5e70a5c03b04a9
User <fadhillatmojo@gmail.com>
2023-11-21 09:08:14 +0700

    fadhil/feat/fetch API Get and Post

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -16,12 +16,12 @@
 	@foreach ($galleries as $gallery)
 	<div class="col-sm-2">
 		<div>
-			<a class="example-image-link" href="{{asset('storage/posts_image/'.$gallery->picture )}}" data-lightbox="roadtrip"
-				data-title="{{$gallery->description}}">
-				<img class="example-image img-fluid mb-2"src="{{asset('storage/posts_image/'.$gallery->picture )}}" alt="image-1" />
+			<a class="example-image-link" href="{{asset('storage/posts_image/'.$gallery['picture'] )}}" data-lightbox="roadtrip"
+				data-title="{{$gallery['description']}}">
+				<img class="example-image img-fluid mb-2"src="{{asset('storage/posts_image/'.$gallery['picture'] )}}" alt="image-1" />
 			</a>
-			<a href="{{ route('gallery.edit', $gallery->id) }}" class="btn btn-primary">Edit</a>
-			<form onsubmit="return confirm('Yakin ingin hapus ?');" action="{{ route('gallery.destroy', $gallery->id) }}" method="POST">
+			<a href="{{ route('gallery.edit', $gallery['id']) }}" class="btn btn-primary">Edit</a>
+			<form onsubmit="return confirm('Yakin ingin hapus ?');" action="{{ route('gallery.destroy', $gallery['id']) }}" method="POST">
 				@csrf
 				@method('DELETE')
 				<button type="submit" class="btn btn-danger my-2">Delete</button>
@@ -32,9 +32,9 @@
 	@else
 	<h3>Tidak ada data.</h3>
 	@endif
-	<div class="d-flex">
+	{{-- <div class="d-flex">
 		{{ $galleries->links() }}
-	</div>
+	</div> --}}
 	<div class="d-flex mt-5">
 		<a href="{{ route('dashboard') }}" class="btn btn-outline-danger">< Back to Home</a>
 	</div>
